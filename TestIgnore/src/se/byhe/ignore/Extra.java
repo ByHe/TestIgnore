53036c022a24b27ee026ebc9f150780b7befe6d1
Bygren <henrik.bygren@gmail.com>
2017-01-20 10:00:11 +0100

    Lite nytt

---
+++ TestIgnore/src/se/byhe/ignore/Extra.java
@@ -0,0 +1,5 @@
+package se.byhe.ignore;
+
+public class Extra {
+
+}
--- TestIgnore/src/se/byhe/ignore/Main.java
+++ TestIgnore/src/se/byhe/ignore/Main.java
@@ -1,10 +1,13 @@
 package se.byhe.ignore;
 
+import java.util.Scanner;
+
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("Nytt");
+		Scanner input = new Scanner(System.in);
+		System.out.println("Nytt på gång");
+		int x = 0;
 	}
 
 }
